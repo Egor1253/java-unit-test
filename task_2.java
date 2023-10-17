bb422d1a491e272bce1e1d7fcf497a82da888f9f
Egor Trofimov <trofimov.egor88@yandex.ru>
2023-10-17 19:32:52 +0300

    My unit tests

---
+++ task_2.java
@@ -0,0 +1,20 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+    assertEquals("Должно вернуться true, так как пользователь совершеннолетний", true, isAdult);
+}
+
+@Test
+public void checkIsAdultWhenAgeIsLessThan18False() { // Напиши код здесь
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(10);
+    assertEquals("Должно вернуться false, так как пользователь не совершеннолетний", false, isAdult);
+}
+
+@Test
+public void checkIsAdultWhenAgeIs18True() { // Напиши код здесь
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(18);
+    assertEquals("Должно вернуться true, так как пользователю 18", true, isAdult);
+}
